7e26d0dff0b2cc64adb4a7ba40b6a5bc978e15d0
wpitek <wojciech.pitek@gmail.com>
2014-08-10 13:44:25 +0200

    added report generator with parameters

---
+++ SharedReports/IReportGenerator.cs
@@ -0,0 +1,11 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+
+namespace SharedReports
+{
+    public interface IReportGenerator
+    {
+        ReportDocument GenerateReport(ReportDocument report, IDictionary<string, object> parameters = null);
+    }
+}
---
+++ SharedReports/ReportGenerator.cs
@@ -0,0 +1,28 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+
+namespace SharedReports
+{
+    public class ReportGenerator : IReportGenerator
+    {
+        public ReportDocument GenerateReport(ReportDocument report, IDictionary<string, object> parameters = null)
+        {
+            if (report == null)
+                throw new ArgumentException("Report cannot be null");
+            AddParametersToReport(report, parameters);
+            return report;
+        }
+
+        private static void AddParametersToReport(ReportDocument report, IDictionary<string, object> parameters)
+        {
+            if (parameters != null)
+            {
+                foreach (var parameter in parameters)
+                {
+                    report.SetParameterValue(parameter.Key, parameter.Value);
+                }
+            }
+        }
+    }
+}
